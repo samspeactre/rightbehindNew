a073025da6c24baa6afdd5cab4640a5c7ead4f63
Ahmed Ali <ahmedakhter1289@gmail.com>
2024-06-05 05:27:56 +0500

    add scroll restoration

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone:true,
+  imports:[RouterModule],
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'

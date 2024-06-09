86df8d454531256aef19a55b7f32b564e8b5dc89
Ahmed Ali <ahmedakhter1289@gmail.com>
2024-06-09 06:05:54 +0500

    changes

--- src/app/SharedComponents/card-carousel/card-carousel.component.ts
+++ src/app/SharedComponents/card-carousel/card-carousel.component.ts
@@ -3,11 +3,13 @@ import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   standalone:true,
-  imports: [RouterModule, MatIconModule, MatButtonModule, CarouselModule],
+  imports: [RouterModule, MatIconModule, MatButtonModule, CarouselModule, FontAwesomeModule],
   selector: 'app-card-carousel',
   templateUrl: './card-carousel.component.html',
   styleUrl: './card-carousel.component.css'
@@ -23,6 +25,8 @@ export class CardCarouselComponent implements OnInit{
     { imgSrc: '../../assets/img/carousel-map-img.png', buttonUrl: '', name: 'Community Title', tag: 'miami', description: 'Lorem ipsum dolor sit amet consectetur. A urna dolor neque quis tortor. Cras auctor mauris tincidunt sed fusce rhoncus.  ' },
     // Add more card data as needed
   ];
+  faChevronCircleLeft=faChevronLeft
+  faChevronCircleRight=faChevronRight
   customOptions: OwlOptions = {
     loop: true,
     mouseDrag: true,

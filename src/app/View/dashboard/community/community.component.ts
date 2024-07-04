12c45e950fa090b1cc1afd74c2309a8f32687da0
Ahmed Ali <ahmedakhter1289@gmail.com>
2024-07-04 21:28:56 +0500

    Update community.component.ts

--- src/app/View/dashboard/community/community.component.ts
+++ src/app/View/dashboard/community/community.component.ts
@@ -10,55 +10,107 @@ import { HttpService } from '../../../Services/http.service';
 import { ChatPopupComponent } from '../../../SharedComponents/add-chat-popup/add-chat-popup.component';
 import { InputComponent } from '../../../SharedComponents/input/input.component';
 import { CommunityCardComponent } from '../../../SharedComponents/community-card/community-card.component';
-import { Subject, distinctUntilChanged, takeUntil } from 'rxjs';
+import { of, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap, takeUntil } from 'rxjs/operators';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, CommunityCardComponent, InputComponent, FormsModule, FontAwesomeModule, ReactiveFormsModule, MiniLoadingComponent],
+  imports: [
+    CommonModule,
+    CommunityCardComponent,
+    InputComponent,
+    FormsModule,
+    FontAwesomeModule,
+    ReactiveFormsModule,
+    MiniLoadingComponent,
+  ],
   selector: 'app-community',
   templateUrl: './community.component.html',
-  styleUrls: ['./community.component.scss']
+  styleUrls: ['./community.component.scss'],
 })
-export class CommunityComponent {
+export class CommunityComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
-  searchForm = this.fb.group({
-    search: ['']
+  searchForm:any = this.fb.group({
+    search: [''],
   });
   faPlus = faPlus;
   inquiries: any;
-  constructor(private fb: FormBuilder, private router: Router, public dialog: MatDialog, private http: HttpService) {
-    this.getInquiries()
+  originalInquiries: any;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    public dialog: MatDialog,
+    private http: HttpService
+  ) {}
+
+  ngOnInit(): void {
+    this.getInquiries();
+    this.initializeSearch();
   }
-  ngOnDestroy() {
+
+  ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
   }
+
+  private initializeSearch(): void {
+    this.searchForm.get('search').valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged(),
+        switchMap((searchTerm: string) => {
+          return of(this.filterInquiries(searchTerm));
+        }),
+        takeUntil(this.destroy$)
+      )
+      .subscribe((result:any) => {
+        // Handle the filtered result
+        console.log(result);
+      });
+  }
+
   openPopup(): void {
     const dialogRef = this.dialog.open(ChatPopupComponent, {
       height: '100%',
-      width: window.innerWidth > 1024 ? '33%' : '100%'
+      width: window.innerWidth > 1024 ? '33%' : '100%',
     });
-    dialogRef.afterClosed().subscribe(result => {
+
+    dialogRef.afterClosed().subscribe((result) => {
       if (result?.data) {
         this.inquiries.push({
-          description:result?.data?.Description,
-          latitude:result?.data?.Latitude,
-          longitude:result?.data?.Longitude,
-          title:result?.data?.Title
-        })
+          description: result.data.Description,
+          latitude: result.data.Latitude,
+          longitude: result.data.Longitude,
+          title: result.data.Title,
+        });
       }
     });
   }
-  getInquiries() {
-    this.http.loaderGet('Forum/get?pageSize=500', true, true)
-    .pipe(
-      takeUntil(this.destroy$),
-      distinctUntilChanged(
-        (prev, curr) => JSON.stringify(prev) === JSON.stringify(curr)
+
+  private getInquiries(): void {
+    this.http
+      .loaderGet('Forum/get?pageSize=500&byUser=true', true, true)
+      .pipe(
+        takeUntil(this.destroy$),
+        distinctUntilChanged((prev, curr) => JSON.stringify(prev) === JSON.stringify(curr))
       )
-    )
-    .subscribe((response) => {
-      this.inquiries = response?.model?.forums
-    })
+      .subscribe((response) => {
+        this.inquiries = response?.model?.forums;
+        this.originalInquiries = response?.model?.forums;
+      });
+  }
+
+  private filterInquiries(searchTerm: string): void {
+    if (!searchTerm) {
+      this.inquiries = this.originalInquiries;
+    } else {
+      this.inquiries = this.originalInquiries.filter((inquiry: any) =>
+        (inquiry.city && inquiry.city.toLowerCase().includes(searchTerm.toLowerCase())) ||
+        (inquiry.address && inquiry.address.toLowerCase().includes(searchTerm.toLowerCase())) ||
+        (inquiry.title && inquiry.title.toLowerCase().includes(searchTerm.toLowerCase())) ||
+        (inquiry.description && inquiry.description.toLowerCase().includes(searchTerm.toLowerCase()))
+      );
+    }
   }
 }

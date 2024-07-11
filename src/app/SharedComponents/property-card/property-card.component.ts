9868fe26db50c09e9878f2709d9e9b4104b79ac3
samspeactre <samspectre1@gmail.com>
2024-07-11 11:16:02 -0700

    Carousel css

--- src/app/SharedComponents/property-card/property-card.component.ts
+++ src/app/SharedComponents/property-card/property-card.component.ts
@@ -12,13 +12,11 @@ import {
   faMapMarkerAlt,
 } from '@fortawesome/free-solid-svg-icons';
 import { MatDialog } from '@angular/material/dialog';
-import { PopupComponent } from '../popup/popup.component';
 import { Store } from '@ngrx/store';
 import { selectUser } from '../../Ngrx/data.reducer';
 import { Subject, distinctUntilChanged, takeUntil } from 'rxjs';
 import { ContactPopupComponent } from '../contact-popup/contact-popup.component';
 import { ResizeService } from '../../Services/resize.service';
-// import { PreviewComponent } from '../../view/preview/preview.component';
 @Component({
   standalone: true,
   imports: [

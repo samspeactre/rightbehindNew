bf128d384a186bd79f9a45e2cf8516adac9c62a3
Ahmed Ali Rajput <ahmedakhter1289@gmail.com>
2024-07-23 04:28:11 +0500

    chnages done contact api remaining

--- src/app/SharedComponents/inquiry-popup/inquiry-popup.component.ts
+++ src/app/SharedComponents/inquiry-popup/inquiry-popup.component.ts
@@ -1,34 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import {
-  FormsModule,
-  ReactiveFormsModule
-} from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import {
   MAT_DIALOG_DATA,
   MatDialog,
   MatDialogRef,
 } from '@angular/material/dialog';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { Store } from '@ngrx/store';
-import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { Subject } from 'rxjs';
 import { selectUser } from '../../Ngrx/data.reducer';
 import { types } from '../../Services/helper.service';
-import { InputComponent } from '../input/input.component';
 import { MapComponent } from '../map/map.component';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
 @Component({
   standalone: true,
   imports: [
-    InputComponent,
-    SweetAlert2Module,
     RouterModule,
-    MatButtonModule,
-    FormsModule,
-    ReactiveFormsModule,
     CommonModule,
     MapComponent,
+    FontAwesomeModule
   ],
   selector: 'app-inquiry-popup',
   templateUrl: './inquiry-popup.component.html',
@@ -40,6 +32,8 @@ export class InquiryPopupComponent {
   user$ = this.store.select(selectUser);
   types = types;
   user: any;
+  faEnvelope=faEnvelope;
+  faPhone=faPhoneAlt
   private destroy$ = new Subject<void>();
   constructor(
     public dialog: MatDialog,
@@ -58,5 +52,12 @@ export class InquiryPopupComponent {
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
+  }
+  route(contactInfo: string, type: string): void {
+    if (type === 'phone') {
+      window.location.href = `tel:${contactInfo}`;
+    } else if (type === 'email') {
+      window.location.href = `mailto:${contactInfo}`;
+    }
   }
 }

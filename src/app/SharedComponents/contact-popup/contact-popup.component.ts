824222e6b19a0f358bd640462eb32e6e3ac91341
Ahmed Ali Rajput <ahmedakhter1289@gmail.com>
2024-07-24 17:11:02 +0500

    Update contact-popup.component.ts

--- src/app/SharedComponents/contact-popup/contact-popup.component.ts
+++ src/app/SharedComponents/contact-popup/contact-popup.component.ts
@@ -1,8 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { Router, RouterModule } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Subject, distinctUntilChanged, of, switchMap, takeUntil } from 'rxjs';
@@ -16,10 +25,19 @@ import Swal from 'sweetalert2';
 import { HttpService } from '../../Services/http.service';
 @Component({
   standalone: true,
-  imports: [InputComponent, SweetAlert2Module, RouterModule, MatButtonModule, FormsModule, ReactiveFormsModule, CommonModule, MapComponent],
+  imports: [
+    InputComponent,
+    SweetAlert2Module,
+    RouterModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    MapComponent,
+  ],
   selector: 'app-contact-popup',
   templateUrl: './contact-popup.component.html',
-  styleUrl: './contact-popup.component.scss'
+  styleUrl: './contact-popup.component.scss',
 })
 export class ContactPopupComponent {
   selected: any;
@@ -30,23 +48,32 @@ export class ContactPopupComponent {
     firstName: ['', Validators.required],
     lastName: ['', Validators.required],
     email: ['', [Validators.required, Validators.email]],
-    message: ['', Validators.required]
+    message: ['', Validators.required],
   });
   user: any;
   private destroy$ = new Subject<void>();
-  constructor(public dialog: MatDialog, public dialogRef: MatDialogRef<ContactPopupComponent>,
-    private router: Router, private fb: FormBuilder, private auth: AuthService,
+  constructor(
+    public dialog: MatDialog,
+    public dialogRef: MatDialogRef<ContactPopupComponent>,
+    private router: Router,
+    private fb: FormBuilder,
+    private auth: AuthService,
     private store: Store,
     private http: HttpService,
     private helper: HelperService,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
-    this.active = data
+    this.active = data;
     if (this.active?.type === 'property') {
-      this.propertyForm.addControl('password', this.fb.control('', [Validators.required, Validators.minLength(8)]));
-    }
-    else{
-      this.propertyForm.addControl('contactNo', this.fb.control('', [Validators.required]));
+      this.propertyForm.addControl(
+        'password',
+        this.fb.control('', [Validators.required, Validators.minLength(8)])
+      );
+    } else {
+      this.propertyForm.addControl(
+        'contactNo',
+        this.fb.control('', [Validators.required])
+      );
     }
   }
   ngOnDestroy(): void {
@@ -58,56 +85,60 @@ export class ContactPopupComponent {
       const data = {
         fullName: `${this.propertyForm.controls['firstName'].value} ${this.propertyForm.controls['lastName'].value}`,
         password: this.propertyForm.controls['password'].value,
-        email: this.propertyForm.controls['email'].value
+        email: this.propertyForm.controls['email'].value,
       };
-      this.auth.register(data).pipe(
-        takeUntil(this.destroy$),
-        switchMap((registerResponse) => this.auth.login(data).pipe(
+      this.auth
+        .register(data)
+        .pipe(
           takeUntil(this.destroy$),
-          switchMap((loginResponse) => {
-            this.auth.handleLoginResponse(loginResponse);
-            return this.helper.createContact(this.active?.id).pipe(
+          switchMap((registerResponse) =>
+            this.auth.login(data).pipe(
               takeUntil(this.destroy$),
-              switchMap((contactResponse) => {
-                const id = contactResponse?.model?.id;
-                const messageData = {
-                  chatContactId: id,
-                  message: this.propertyForm.controls['message'].value
-                };
-                if (id) {
-                  return this.http.loaderPost('Chat/send', messageData, true).pipe(
-                    takeUntil(this.destroy$)
-                  );
-                } else {
-                  return of(null);
-                }
+              switchMap((loginResponse) => {
+                this.auth.handleLoginResponse(loginResponse);
+                return this.helper.createContact(this.active?.id).pipe(
+                  takeUntil(this.destroy$),
+                  switchMap((contactResponse) => {
+                    const id = contactResponse?.model?.id;
+                    const messageData = {
+                      chatContactId: id,
+                      message: this.propertyForm.controls['message'].value,
+                    };
+                    if (id) {
+                      return this.http
+                        .loaderPost('Chat/send', messageData, true)
+                        .pipe(takeUntil(this.destroy$));
+                    } else {
+                      return of(null);
+                    }
+                  })
+                );
               })
-            );
-          })
-        ))
-      ).subscribe({
-        next: () => {
-          this.propertyForm.reset();
-          this.dialogRef.close();
-        },
-        error: (err) => {
-          console.error('An error occurred:', err);
-        }
-      });
-    }
-    else{
+            )
+          )
+        )
+        .subscribe({
+          next: () => {
+            this.propertyForm.reset();
+            this.dialogRef.close();
+          },
+          error: (err) => {
+            console.error('An error occurred:', err);
+          },
+        });
+    } else {
       const data = {
         fullName: `${this.propertyForm.controls['firstName'].value} ${this.propertyForm.controls['lastName'].value}`,
         email: this.propertyForm.controls['email'].value,
-        contactNo:this.propertyForm.controls['contactNo'].value,
-        message: this.propertyForm.controls['message'].value
+        contactNo: this.propertyForm.controls['contactNo'].value,
+        message: this.propertyForm.controls['message'].value,
       };
       this.http
-          .loaderPost('ContactUs/create', data, true)
-          .subscribe((response: any) => {
-            this.propertyForm.reset();
+        .loaderPost('ContactUs/create', data, true)
+        .subscribe((response: any) => {
+          this.propertyForm.reset();
           this.dialogRef.close();
-          });
+        });
     }
   }
 }

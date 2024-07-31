dfcce7564ca50b3e0b1a0ccbeabe7fa2a8366147
Ahmed Ali Rajput <ahmedakhter1289@gmail.com>
2024-07-31 12:56:07 +0500

    wokring on map

--- src/app/SharedComponents/dummy-map/dummy-map.component.ts
+++ src/app/SharedComponents/dummy-map/dummy-map.component.ts
@@ -1,7 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  ApplicationRef,
+  Component,
+  ComponentFactoryResolver,
+  EventEmitter,
+  Injector,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faEdit } from '@fortawesome/free-regular-svg-icons';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { PropertyCardMapComponent } from '../property-card-map/property-card-map.component';
+import { CommunityCardMapComponent } from '../community-card-map/community-card-map.component';
+import { ResizeService } from '../../Services/resize.service';
 
 declare var google: any;
 
@@ -15,12 +27,73 @@ declare var google: any;
 export class DummyMapComponent implements OnInit {
   map: any;
   poly: any;
+  private _center: google.maps.LatLngLiteral = {
+    lat: 25.761681,
+    lng: -80.191788,
+  };
+  @Input() set highlighted(data: any) {
+    if (data) {
+      this.zoomToHighlightedMarker(data);
+    }
+  }
+  @Input()
+  get center(): google.maps.LatLngLiteral {
+    return this._center;
+  }
+
+  set center(value: google.maps.LatLngLiteral) {
+    this._center = value;
+    if (this.mapOptions) {
+      this.mapOptions.center = this._center;
+    }
+    if (this.map) {
+      this.map.setCenter(this._center);
+    }
+  }
+  @Input() infoContents: any[] = [];
   @Input() height: any;
+  @Input() community: boolean = false;
+  @Output() propertyHover = new EventEmitter<any>();
+  @Output() drawCordinates = new EventEmitter<any>();
+  @Input() set markerPositions(data: any[]) {
+    if (data) {
+      this.markers = data;
+      this.placeMarkers();
+    }
+  }
+  @Input() set communityMarkerPositions(data: any[]) {
+    if (data) {
+      this.communityMarkers = data;
+      this.placeMarkers();
+    }
+  }
+
+  markers: any[] = [];
+  communityMarkers: any[] = [];
+  originalMarkers: any[] = [];
+  googleMarkers: any[] = [];
   faEdit = faEdit;
   faTrash = faTrash;
   drawing: boolean = false;
-
-  constructor() {}
+  private currentInfoWindow: google.maps.InfoWindow | null = null;
+  mapOptions: any = {
+    zoom: 13,
+    center: this._center,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    gestureHandling: 'greedy',
+    draggable: true,
+    mapTypeControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    rotateControl: false,
+    fullscreenControl: false,
+  };
+  constructor(
+    private resolver: ComponentFactoryResolver,
+    private injector: Injector,
+    private appRef: ApplicationRef,
+    public resize: ResizeService
+  ) {}
 
   ngOnInit(): void {
     this.initializeMap();
@@ -61,8 +134,7 @@ export class DummyMapComponent implements OnInit {
         lat: latLng.lat(),
         lng: latLng.lng(),
       }));
-      console.log(coordinates);
-
+      this.drawCordinates.emit(JSON.stringify(coordinates));
       this.poly.setMap(null);
       this.poly = new google.maps.Polygon({ map: this.map, path: path });
       google.maps.event.clearListeners(this.map.getDiv(), 'mousedown');
@@ -96,25 +168,18 @@ export class DummyMapComponent implements OnInit {
       this.poly.setMap(null);
       this.poly = null;
     }
+    this.placeMarkers();
     this.drawing = false;
   }
 
   initializeMap(): void {
-    const mapOptions = {
-      zoom: 14,
-      center: new google.maps.LatLng(52.5498783, 13.425209099999961),
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-    };
-
-    this.map = new google.maps.Map(
-      document.getElementById('map_canvas'),
-      mapOptions
-    );
-
+    this.map = new google.maps.Map(document.getElementById('map_canvas'));
+    this.map.setOptions(this.mapOptions);
     document.getElementById('drawpoly').addEventListener('click', (e) => {
       e.preventDefault();
       this.disable();
       this.drawing = true;
+      this.clearMarkers();
       google.maps.event.addDomListener(
         this.map.getDiv(),
         'mousedown',
@@ -136,5 +201,92 @@ export class DummyMapComponent implements OnInit {
       e.preventDefault();
       this.clearShapes();
     });
+    this.placeMarkers();
+  }
+  zoomToHighlightedMarker(highlighted: any) {
+    if (!this.map || !highlighted) return;
+
+    const marker =
+      this.markers.find(
+        (m) => m?.lat == highlighted.lat && m?.lng == highlighted.lng
+      ) ||
+      this.communityMarkers.find(
+        (m) => m?.lat === highlighted?.lat && m?.lng === highlighted?.lng
+      );
+
+    if (marker && marker.markerInstance) {
+      const position = marker.markerInstance.getPosition();
+      if (position) {
+        this.map.setCenter(position);
+
+        // const currentIcon = marker.markerInstance.getIcon();
+
+        // marker.markerInstance.setIcon({
+        //   url: currentIcon.url || currentIcon,
+        //   scaledSize: new google.maps.Size(70, 70)
+        // });
+
+        // setTimeout(() => {
+        //   marker.markerInstance.setIcon({
+        //     url: currentIcon.url || currentIcon,
+        //     scaledSize: currentIcon.scaledSize || new google.maps.Size(50, 50)
+        //   });
+        // }, 3000);
+      }
+    }
+  }
+  placeMarkers() {
+    this.clearMarkers();
+    this.createMarkers(this.markers, '/assets/img/propertyMar.webp');
+    this.createMarkers(this.communityMarkers, '/assets/img/markerC.webp');
+  }
+
+  createMarkers(markerDataArray: any[], iconUrl: string) {
+    markerDataArray.forEach((markerData, index) => {
+      const marker = new google.maps.Marker({
+        position: new google.maps.LatLng(markerData.lat, markerData.lng),
+        map: this.map,
+        icon: iconUrl,
+      });
+      const infoWindow = new google.maps.InfoWindow({
+        content: this.createInfoWindowContent(index),
+      });
+
+      marker.addListener('click', () => {
+        if (this.currentInfoWindow) {
+          this.currentInfoWindow.close();
+        }
+        infoWindow.open(this.map, marker);
+        this.currentInfoWindow = infoWindow;
+        this.propertyHover.emit(
+          this.infoContents[index]?.listingId || this.infoContents[index]?.id
+        );
+        this.map.setCenter(marker.getPosition());
+        this.map.setZoom(13);
+      });
+      markerData.markerInstance = marker;
+      markerData.infoWindowInstance = infoWindow;
+      this.googleMarkers.push(marker);
+    });
+  }
+
+  createInfoWindowContent(index: number): HTMLElement {
+    const component: any = this.community
+      ? CommunityCardMapComponent
+      : PropertyCardMapComponent;
+    const factory = this.resolver.resolveComponentFactory(component);
+    const componentRef: any = factory.create(this.injector);
+    componentRef.instance.card = this.infoContents[index];
+    componentRef.instance.loader = false;
+    componentRef.instance.routeDirect = true;
+    this.appRef.attachView(componentRef.hostView);
+    const div = document.createElement('div');
+    div.appendChild(componentRef.location.nativeElement);
+    return div;
+  }
+
+  clearMarkers() {
+    this.googleMarkers.forEach((marker) => marker.setMap(null));
+    this.googleMarkers = [];
   }
 }

270854fcc3f841610eeb4e8d70fff058b9c6a562
Ahmed Ali Rajput <ahmedakhter1289@gmail.com>
2024-08-01 09:10:40 +0500

    changes fixed

--- src/app/SharedComponents/popup/popup.component.ts
+++ src/app/SharedComponents/popup/popup.component.ts
@@ -1,9 +1,5 @@
 import { CommonModule } from '@angular/common';
-import {
-  Component,
-  Inject,
-  OnInit
-} from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {
   MAT_DIALOG_DATA,
@@ -14,7 +10,12 @@ import {
 import { MatIconModule } from '@angular/material/icon';
 import { Router, RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faEllipsisVertical, faHeart, faShare, faShareAlt } from '@fortawesome/free-solid-svg-icons';
+import {
+  faEllipsisVertical,
+  faHeart,
+  faShare,
+  faShareAlt,
+} from '@fortawesome/free-solid-svg-icons';
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 import { Store } from '@ngrx/store';
 import { distinctUntilChanged, Subject, takeUntil } from 'rxjs';
@@ -25,19 +26,31 @@ import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 import { ResizeService } from '../../Services/resize.service';
 import { ContactPopupComponent } from '../contact-popup/contact-popup.component';
 import { selectUser } from '../../Ngrx/data.reducer';
+import { ContactSelectComponent } from '../contact-select/contact-select.component';
+import { NoopScrollStrategy } from '@angular/cdk/overlay';
 
 @Component({
   standalone: true,
-  imports: [MatIconModule, MapComponent, CarouselModule, CommonModule, MatDialogContent, MatButtonModule, RouterModule,FontAwesomeModule, NgbTooltipModule],
+  imports: [
+    MatIconModule,
+    MapComponent,
+    CarouselModule,
+    CommonModule,
+    MatDialogContent,
+    MatButtonModule,
+    RouterModule,
+    FontAwesomeModule,
+    NgbTooltipModule,
+  ],
   selector: 'app-popup',
   templateUrl: './popup.component.html',
   styleUrl: './popup.component.scss',
 })
 export class PopupComponent implements OnInit {
-  faHeart=faHeart;
-  faShare=faShareAlt
-  faEllipsisVertical=faEllipsisVertical
-  faRedirect=faShare
+  faHeart = faHeart;
+  faShare = faShareAlt;
+  faEllipsisVertical = faEllipsisVertical;
+  faRedirect = faShare;
   propertyData: any;
   user$ = this.store.select(selectUser);
   userDetails: any;
@@ -49,8 +62,8 @@ export class PopupComponent implements OnInit {
     private http: HttpService,
     private dialog: MatDialog,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    public helper:HelperService,
-    public resize:ResizeService
+    public helper: HelperService,
+    public resize: ResizeService
   ) {
     this.propertyData = data?.card;
     this.user$
@@ -85,13 +98,12 @@ export class PopupComponent implements OnInit {
     navText: ['', ''],
     responsive: {
       0: {
-        items: 1
-      }
+        items: 1,
+      },
     },
-    nav: false
-  }
-  ngOnInit() {
-  }
+    nav: false,
+  };
+  ngOnInit() {}
 
   onHeadingClick(id: string) {
     document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
@@ -106,19 +118,51 @@ export class PopupComponent implements OnInit {
   }
 
   navigateAndClose() {
-    this.dialogRef.close();
+    this.dialog.closeAll();
     setTimeout(() => {
-      this.router.navigate(
-        ['/preview'],
-        { queryParams: { id: this.propertyData?.listingId || this.propertyData?.id, type: this.propertyData?.listingId ? 'mls' : '2' } }
-      );
+      this.router.navigate(['/preview'], {
+        queryParams: {
+          id: this.propertyData?.listingId || this.propertyData?.id,
+          type: this.propertyData?.listingId ? 'mls' : '2',
+        },
+      });
     });
+  }
+  openContactOptions() {
+    const dialogRef = this.dialog.open(ContactSelectComponent, {
+      width: window.innerWidth > 1024 ? '500px' : '100%',
+      data: {
+        email: this.propertyData.listAgentEmail,
+        phone: this.propertyData.listAgentOfficePhone,
+      },
+      scrollStrategy: new NoopScrollStrategy(),
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result === 'email' && this.propertyData.listAgentEmail) {
+        this.openEmail();
+      } else if (result === 'phone' && this.propertyData.listAgentOfficePhone) {
+        this.openPhone();
+      }
+    });
+  }
+  openEmail() {
+    window.location.href = `mailto:${this.propertyData.listAgentEmail}`;
+  }
+
+  openPhone() {
+    window.location.href = `tel:${this.propertyData.listAgentOfficePhone}`;
   }
   async share() {
     try {
-      await navigator.share({ title: this.propertyData?.title, url: `preview/?id=${this.propertyData?.listingId || this.propertyData?.id}&type=2` });
+      await navigator.share({
+        title: this.propertyData?.title,
+        url: `preview/?id=${
+          this.propertyData?.listingId || this.propertyData?.id
+        }&type=2`,
+      });
     } catch (err: any) {
-      console.error("Share failed:", err?.message);
+      console.error('Share failed:', err?.message);
     }
   }
 
@@ -141,13 +185,14 @@ export class PopupComponent implements OnInit {
         },
         contactId: response?.model?.id,
       };
+      this.dialog.closeAll();
       this.router.navigate(['/dashboard/inquiries'], {
         queryParams: { data: JSON.stringify(routeData) },
       });
     });
   }
   openPopup(): void {
-    this.dialog.closeAll()
+    this.dialog.closeAll();
     this.dialog?.open(ContactPopupComponent, {
       width: window.innerWidth > 1024 ? '420px' : '100%',
       data: { type: 'property', id: this.propertyData?.id },
